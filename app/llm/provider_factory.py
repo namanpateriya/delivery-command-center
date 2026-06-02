d67c8eacd97b13139a2e9c286b2e6f9889fd6f1a
namanpateriya <pateriyanaman@gmail.com>
2026-06-02 19:37:53 +0530

    Create provider_factory.py

---
+++ app/llm/provider_factory.py
@@ -0,0 +1,46 @@
+from app.config import (
+    LLM_PROVIDER
+)
+
+from app.llm.providers.gemini_provider import (
+    GeminiProvider
+)
+
+from app.llm.providers.openai_provider import (
+    OpenAIProvider
+)
+
+from app.llm.providers.anthropic_provider import (
+    AnthropicProvider
+)
+
+from app.llm.providers.bedrock_provider import (
+    BedrockProvider
+)
+
+
+class ProviderFactory:
+
+    @staticmethod
+    def get_provider():
+
+        if LLM_PROVIDER == "gemini":
+
+            return GeminiProvider()
+
+        if LLM_PROVIDER == "openai":
+
+            return OpenAIProvider()
+
+        if LLM_PROVIDER == "anthropic":
+
+            return AnthropicProvider()
+
+        if LLM_PROVIDER == "bedrock":
+
+            return BedrockProvider()
+
+        raise ValueError(
+            f"Unsupported provider: "
+            f"{LLM_PROVIDER}"
+        )
