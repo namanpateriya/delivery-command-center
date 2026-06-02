8dcb8a8ee086870b0450174e65eb007ba297ae24
namanpateriya <pateriyanaman@gmail.com>
2026-06-02 19:01:20 +0530

    Create planner.py

---
+++ app/orchestrator/planner.py
@@ -0,0 +1,12 @@
+class WorkflowPlanner:
+
+    def create_plan(
+        self,
+        user_query: str
+    ):
+
+        return [
+            "delivery",
+            "risk",
+            "communication"
+        ]
